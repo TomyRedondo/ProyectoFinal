7620bef90bd3447c77c1ad1f9816fb18481d68da
Tomas Redondo <tomas.redondo27@gmail.com>
2024-01-22 20:26:00 -0300

    urls2

--- src/myblog/blog/urls.py
+++ src/myblog/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from blog.views import index, animes, mangas, videojuegos, usuarios_manga
+from blog.views import index, animes, videojuegos, usuarios_manga
 
 urlpatterns = [
     path('', index, name='index'),
     path('anime/', animes, name='animes'),
-    path('manga/', mangas, name='mangas'),
+    # path('manga/', mangas, name='mangas'),
     path('videojuego/', videojuegos, name='videojuegos'),
     path('usuario/', usuarios_manga, name='usuarios_manga'),
 ]
